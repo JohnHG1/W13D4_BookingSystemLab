52e74d853398b399a7dc978c50a10af80e532690
JohnHG1 <honisz@yahoo.com>
2020-04-09 12:00:33 +0100

    Second model done

--- src/main/java/com/codeclan/example/Booking/System/models/Course.java
+++ src/main/java/com/codeclan/example/Booking/System/models/Course.java
@@ -1,4 +1,54 @@
 package com.codeclan.example.Booking.System.models;
 
+import com.sun.xml.internal.xsom.impl.scd.Step;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+
+    private String name;
+    private String town;
+    private int starRating;
+    private List<Booking> bookings;
+
+    public Course(String name, String town, int starRating) {
+        this.name = name;
+        this.town = town;
+        this.starRating = starRating;
+        this.bookings = new ArrayList<>();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
